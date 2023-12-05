19a7390502a9dbf9f2774c28d4af31ac27adcaa3
Devlop3rHardik <avaghela1997@gmail.com>
2023-12-05 19:57:08 +0530

    Jay Bholad Vala Dada

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+let counter = 0;
+let uniqueUsers = new Set();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  const userIP = req.ip;
+
+  if (!uniqueUsers.has(userIP)) {
+    // Increment the counter only if it's a new user
+    counter++;
+    uniqueUsers.add(userIP);
+    console.log(`New user detected. Counter incremented to ${counter}`);
+  }
+
+  next();
+});
+
+app.get('/counter', (req, res) => {
+  res.json({ counter });
+});
+
+// Add a new route to serve the HTML page
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
